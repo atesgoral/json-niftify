aa3c331a7ab94134218e60c885ae53e2fccb25f1
Ates Goral <ates.g@myplanet.com>
2015-10-22 13:08:04 -0400

    Added tests for primitives

--- test/test-json-niftify.js
+++ test/test-json-niftify.js
@@ -1,6 +1,29 @@
 var niftify = require('../index');
 
+exports.testSerializeUndefined = function (test) {
+    test.strictEqual(niftify(undefined), undefined);
+    test.done();
+};
+
+exports.testSerializeNull = function (test) {
+    test.strictEqual(niftify(null), 'null');
+    test.done();
+};
+
+
 exports.testSerializeNumber = function (test) {
     test.strictEqual(niftify(42), '42');
     test.done();
 };
+
+exports.testSerializeString = function (test) {
+    test.strictEqual(niftify('A'), '"A"');
+    test.done();
+};
+
+exports.testSerializeBoolean = function (test) {
+    test.strictEqual(niftify(true), 'true');
+    test.strictEqual(niftify(false), 'false');
+    test.done();
+};
+
